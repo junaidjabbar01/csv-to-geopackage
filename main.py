1b15ac13fd869b81fd27c66634569a868d2e1c9a
Admin <junaid.abdul.jabbar@gmail.com>
2024-04-29 16:04:55 +0100

    added xls to csv

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import glob
 import os
 from osgeo import ogr
+import pandas as pd
 
 gpkg_path = r'C:\Users\Admin\PycharmProjects\csv-to-geopackage\empty_geopackage.gpkg'
-path = r'C:\Users\Admin\PycharmProjects\csv-to-geopackage'
+path = r'C:\Users\Admin\PycharmProjects\csv-to-geopackage\data\data_csv'
 # Create a new GeoPackage datasource
 driver = ogr.GetDriverByName('GPKG')
 datasource = driver.CreateDataSource(gpkg_path)
+
+def split_excel_to_csv(excel_file):
+    # Load Excel file
+    xls = pd.ExcelFile(excel_file)
+    # Create a directory to store CSV files
+    output_dir = os.path.splitext(excel_file)[0] + "_csv"
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Iterate over each sheet and write to CSV
+    for sheet_name in xls.sheet_names:
+        df = xls.parse(sheet_name)
+        csv_file = os.path.join(output_dir, f"{sheet_name}.csv")
+        df.to_csv(csv_file, index=False)
+        print(f"Sheet '{sheet_name}' saved as '{csv_file}'")
+
+excel_file_path = r"C:\Users\Admin\PycharmProjects\csv-to-geopackage\data\data_3.xlsx"  # Replace with the path to your Excel file
+split_excel_to_csv(excel_file_path)
 
 if datasource is None:
     print("Creation of GeoPackage file failed.")
@@ -18,9 +36,7 @@ if datasource is None:
 #spatial_reference.ImportFromEPSG(4326)  # WGS84
 #layer = datasource.CreateLayer(layer_name, spatial_reference, ogr.wkbPoint)
 
-
 for csv in glob.glob(path + '/*.csv'):
-    
     csv_file = 'file:///' + csv + '?delimiter=,'
     table = QgsVectorLayer(csv_file, os.path.basename(csv), 'delimitedtext')
     opt = QgsVectorFileWriter.SaveVectorOptions()
